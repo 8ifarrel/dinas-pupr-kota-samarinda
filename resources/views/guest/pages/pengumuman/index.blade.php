18382d9196714258bf91c718fdb39e18909d6f4a
8ifarrel <farrelsirah@gmail.com>
2024-07-24 15:34:01 +0800

    pengumuman v1.1

--- resources/views/guest/pages/pengumuman/index.blade.php
+++ resources/views/guest/pages/pengumuman/index.blade.php
@@ -96,8 +96,7 @@
         <!-- Modal footer -->
         <div class="flex items-center p-4 md:p-5 border-t border-gray-200 rounded-b dark:border-gray-600">
           <button data-modal-hide="static-modal" type="button"
-            class="text-white bg-blue hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">I
-            Unduh Lampiran</button>
+            class="text-white bg-blue hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Unduh Lampiran</button>
         </div>
       </div>
     </div>
