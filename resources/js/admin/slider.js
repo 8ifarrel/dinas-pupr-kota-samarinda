c442be8a12ffded05776da953fbedb2c72fc2142
8ifarrel <farrelsirah@gmail.com>
2025-01-02 16:43:35 +0800

    Menambahkan TODO Slider dan mempersingkat code

--- resources/js/admin/slider.js
+++ resources/js/admin/slider.js
@@ -5,7 +5,7 @@ import "cropperjs/dist/cropper.css";
 
 FilePond.registerPlugin(FilePondPluginImagePreview);
 
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", () => {
     const fileInput = document.querySelector('input[name="foto_slider"]');
     const editButton = document.querySelector("#edit-image-button");
     const cropperModalElement = document.getElementById("cropperModal");
@@ -17,62 +17,53 @@ document.addEventListener("DOMContentLoaded", function () {
 
     const cropperModal = new Modal(cropperModalElement);
 
+    const csrfToken = document.head.querySelector(
+        'meta[name="csrf-token"]'
+    ).content;
+
+
+    /**
+     * TODO:
+     * 1. Tambahkan code yang dapat menghapus file jika me-refresh halaman
+     */
     const pond = FilePond.create(fileInput, {
         allowMultiple: false,
         acceptedFileTypes: ["image/*"],
-        labelIdle: 'Seret foto ke sini atau <span class="filepond--label-action">telusuri foto</span>',
+        labelIdle:
+            'Seret foto ke sini atau <span class="filepond--label-action">telusuri foto</span>',
         server: {
             process: {
                 url: "/filepond/process",
                 method: "POST",
-                headers: {
-                    "X-CSRF-TOKEN": document.head.querySelector(
-                        'meta[name="csrf-token"]'
-                    ).content,
-                },
+                headers: { "X-CSRF-TOKEN": csrfToken },
             },
             revert: {
                 url: "/filepond/revert",
                 method: "DELETE",
                 headers: {
-                    "X-CSRF-TOKEN": document.head.querySelector(
-                        'meta[name="csrf-token"]'
-                    ).content,
+                    "X-CSRF-TOKEN": csrfToken,
                     "Content-Type": "application/json",
                 },
             },
         },
         onprocessfile: (error, file) => {
             if (!error) {
-                const serverId = file.serverId;
-                uploadedFileUrl = JSON.parse(serverId).fileUrl;
+                uploadedFileUrl = JSON.parse(file.serverId).fileUrl;
                 editButton.style.display = "inline-block";
             }
         },
-        onprocessfilerevert: (file) => {
+        onprocessfilerevert: () => {
             uploadedFileUrl = null;
             editButton.style.display = "none";
         },
     });
 
-    window.addEventListener("beforeunload", () => {
-        if (uploadedFileUrl) {
-            navigator.sendBeacon(
-                "/filepond/revert",
-                JSON.stringify({ fileUrl: uploadedFileUrl })
-            );
-        }
-    });
-
     if (editButton) {
-        editButton.addEventListener("click", function () {
-            const imageUrl = uploadedFileUrl;
-            if (imageUrl) {
-                imageToCrop.src = imageUrl;
+        editButton.addEventListener("click", () => {
+            if (uploadedFileUrl) {
+                imageToCrop.src = uploadedFileUrl;
                 cropperModal.show();
-                if (cropper) {
-                    cropper.destroy();
-                }
+                if (cropper) cropper.destroy();
                 cropper = new Cropper(imageToCrop, {
                     aspectRatio: 21 / 9,
                     viewMode: 1,
@@ -81,23 +72,24 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     }
 
-    cropButton.addEventListener("click", function () {
+    /**
+     * TODO:
+     * 1. Tambahkan code untuk menyesuaikan extensi file yang diupload
+     */
+    cropButton.addEventListener("click", () => {
         if (cropper) {
-            const canvas = cropper.getCroppedCanvas();
-            canvas.toBlob((blob) => {
+            cropper.getCroppedCanvas().toBlob((blob) => {
                 const file = new File([blob], "cropped_image.jpg", {
                     type: "image/jpeg",
                 });
-                // Remove the previous file from FilePond using revert
                 fetch("/filepond/revert", {
                     method: "DELETE",
                     headers: {
-                        "X-CSRF-TOKEN": document.head.querySelector('meta[name="csrf-token"]').content,
+                        "X-CSRF-TOKEN": csrfToken,
                         "Content-Type": "application/json",
                     },
                     body: JSON.stringify({ fileUrl: uploadedFileUrl }),
                 }).then(() => {
-                    pond.removeFile(pond.getFiles()[0].id);
                     pond.addFile(file);
                     cropperModal.hide();
                     cropper.destroy();
