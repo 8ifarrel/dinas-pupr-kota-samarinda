6d6e3be54c53bfad223e5b994b0bec17d4f6efc6
8ifarrel <farrelsirah@gmail.com>
2025-01-15 19:29:57 +0800

    Error message untuk Tambah Pegawai

--- resources/views/admin/pages/pegawai/create.blade.php
+++ resources/views/admin/pages/pegawai/create.blade.php
@@ -7,16 +7,35 @@
 
 @section('slot')
   <div class="w-full p-4 rounded-lg shadow-xl sm:p-8 mt-5 border">
+    @if ($errors->any())
+      <div id="alert-border-2" class="flex items-center p-4 mb-4 text-red-800 border-t-4 border-red-300 bg-red-50 dark:text-red-400 dark:bg-gray-800 dark:border-red-800" role="alert">
+        <svg class="flex-shrink-0 w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+          <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z"/>
+        </svg>
+        <div class="ms-3 text-sm font-medium">
+          @foreach ($errors->all() as $error)
+            <p>{{ $error }}</p>
+          @endforeach
+        </div>
+        <button type="button" class="ms-auto -mx-1.5 -my-1.5 bg-red-50 text-red-500 rounded-lg focus:ring-2 focus:ring-red-400 p-1.5 hover:bg-red-200 inline-flex items-center justify-center h-8 w-8 dark:bg-gray-800 dark:text-red-400 dark:hover:bg-gray-700"  data-dismiss-target="#alert-border-2" aria-label="Close">
+          <span class="sr-only">Dismiss</span>
+          <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+          </svg>
+        </button>
+      </div>
+    @endif
+
     <form action="{{ route('admin.pegawai.store') }}" method="POST" enctype="multipart/form-data" autocomplete="off">
       @csrf
       
       <h2 class="font-semibold text-2xl mb-5 md:text-3xl">
-        Tambah Pegawai
+        Profil Pegawai
       </h2>
 
       <div class="mb-4">
-        <label for="id_jabatan" class="block font-medium text-gray-700">Jabatan</label>
-        <select name="id_jabatan" id="id_jabatan" class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500 sm:text-sm text-gray-900 bg-gray-50" required>
+        <label for="id_jabatan" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Jabatan</label>
+        <select name="id_jabatan" id="id_jabatan" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
           @foreach ($jabatan as $jabatanItem)
             <option value="{{ $jabatanItem->id_jabatan }}">{{ $jabatanItem->nama_jabatan }}</option>
           @endforeach
