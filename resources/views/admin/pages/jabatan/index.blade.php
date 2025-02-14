d5cfaf54d25763652648ea2a952169a3b0025cce
8ifarrel <farrelsirah@gmail.com>
2025-02-14 10:22:26 +0800

    Mengembalikan fitur delete Jabatan

--- resources/views/admin/pages/jabatan/index.blade.php
+++ resources/views/admin/pages/jabatan/index.blade.php
@@ -52,6 +52,13 @@
                       <i class="fa-solid fa-users"></i> <span class="font-medium whitespace-nowrap text-xs sm:text-sm">Lihat Pegawai</span>
                     </a>
                   @endif
+                  @if ($item->id_jabatan != 1)
+                    <button data-modal-target="deleteModal-{{ $item->id_jabatan }}"
+                      data-modal-toggle="deleteModal-{{ $item->id_jabatan }}"
+                      class="flex justify-center items-center w-10 h-10 text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 rounded-lg text-sm p-2.5 focus:outline-none">
+                      <i class="fa-solid fa-trash-can"></i>
+                    </button>
+                  @endif
                 </div>
               </td>
             </tr>
@@ -86,6 +93,50 @@
                 </div>
               </div>
             </div>
+
+            <!-- Modal Konfirmasi Hapus -->
+            @if ($item->id_jabatan != 1)
+            <div id="deleteModal-{{ $item->id_jabatan }}" data-modal-backdrop="static" tabindex="-1" aria-hidden="true"
+              class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+              <div class="relative p-4 w-full max-w-2xl max-h-full">
+                <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                  <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
+                    <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
+                      Konfirmasi Penghapusan
+                    </h3>
+                    <button type="button"
+                      class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+                      data-modal-hide="deleteModal-{{ $item->id_jabatan }}">
+                      <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+                        viewBox="0 0 14 14">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                          d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+                      </svg>
+                      <span class="sr-only">Close modal</span>
+                    </button>
+                  </div>
+                  <div class="p-4 md:p-5 space-y-4">
+                    <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+                      Apakah Anda yakin ingin menghapus jabatan <strong>{{ $item->nama_jabatan }}</strong>? Anda secara otomatis akan juga menghapus pegawai, subbagian, dan jabatan fungsional yang ada pada {{ $item->nama_jabatan }}
+                    </p>
+                  </div>
+                  <div class="flex items-center p-4 md:p-5 border-t border-gray-200 rounded-b dark:border-gray-600">
+                    <form action="{{ route('admin.jabatan.destroy', $item->id_jabatan) }}" method="POST">
+                      @csrf
+                      @method('DELETE')
+                      <button type="submit"
+                        class="text-white bg-red-600 hover:bg-red-700 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800">Hapus</button>
+                    </form>
+                    <button type="button"
+                      class="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+                      data-modal-hide="deleteModal-{{ $item->id_jabatan }}">
+                      Tidak
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+            @endif
           @endforeach
         </tbody>
         <tfoot>
@@ -124,7 +175,56 @@
                 <td class="p-1">
                   <a href="${editRoute.replace(':id', child.id_jabatan)}" class="flex justify-center items-center w-9 h-9 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 rounded-lg text-sm p-2.5 focus:outline-none"><i class="fa-solid fa-pencil"></i></a>
                 </td>
+                <td class="p-1">
+                  <button data-modal-target="deleteModal-${child.id_jabatan}"
+                    data-modal-toggle="deleteModal-${child.id_jabatan}"
+                    class="flex justify-center items-center w-9 h-9 text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 rounded-lg text-sm p-2.5 focus:outline-none">
+                    <i class="fa-solid fa-trash-can"></i>
+                  </button>
+                </td>
             </tr>
+
+            <!-- Modal Konfirmasi Hapus -->
+            <div id="deleteModal-${child.id_jabatan}" data-modal-backdrop="static" tabindex="-1" aria-hidden="true"
+              class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+              <div class="relative p-4 w-full max-w-2xl max-h-full">
+                <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                  <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
+                    <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
+                      Konfirmasi Penghapusan
+                    </h3>
+                    <button type="button"
+                      class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+                      data-modal-hide="deleteModal-${child.id_jabatan}">
+                      <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+                        viewBox="0 0 14 14">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                          d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+                      </svg>
+                      <span class="sr-only">Close modal</span>
+                    </button>
+                  </div>
+                  <div class="p-4 md:p-5 space-y-4">
+                    <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+                      Apakah Anda yakin ingin menghapus jabatan <strong>${child.nama_jabatan}</strong>? Anda secara otomatis akan juga menghapus pegawai yang ada pada ${child.nama_jabatan}
+                    </p>
+                  </div>
+                  <div class="flex items-center p-4 md:p-5 border-t border-gray-200 rounded-b dark:border-gray-600">
+                    <form action="{{ route('admin.jabatan.destroy', child.id_jabatan) }}" method="POST">
+                      @csrf
+                      @method('DELETE')
+                      <button type="submit"
+                        class="text-white bg-red-600 hover:bg-red-700 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800">Hapus</button>
+                    </form>
+                    <button type="button"
+                      class="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+                      data-modal-hide="deleteModal-${child.id_jabatan}">
+                      Tidak
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
         `).join('');
 
         return `<table class="table-auto w-full">${children}</table>`;
