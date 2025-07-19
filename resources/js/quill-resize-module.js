74cbd47c5cb38ede804d4ee0a304e47248c6ffdd
8ifarrel <farrelsirah@gmail.com>
2025-07-19 23:14:55 +0800

    ngetes lagi wkwk x101

--- resources/js/quill-resize-module.js
+++ resources/js/quill-resize-module.js
@@ -1,7 +1,9 @@
-import QuillResize from 'quill-resize-module/dist/resize.js';
-
-if (window.Quill) {
-    window.Quill.register('modules/resize', QuillResize);
+if (typeof window !== 'undefined') {
+    import('quill-resize-module/dist/resize.js').then(module => {
+        const QuillResize = module.default || module;
+        if (window.Quill) {
+            window.Quill.register('modules/resize', QuillResize);
+        }
+        window.QuillResize = QuillResize;
+    }).catch(console.error);
 }
-
-window.QuillResize = QuillResize;
