d76fffbe978d572423c455258cfc6455df858c01
8ifarrel <farrelsirah@gmail.com>
2025-07-31 19:29:29 +0800

    tempat ngoding view

---
+++ resources/views/guest/pages/jalan-peduli/bikin-view-di-sini.blade.php
@@ -0,0 +1,13 @@
+@extends('guest.layouts.main')
+
+@section('document.start')
+{{-- ini <head> --}}
+@endsection
+
+@section('document.body')
+--- ini konten ---
+@endsection
+
+@section('document.end')
+{{-- ini <body> bawah --}}
+@endsection
