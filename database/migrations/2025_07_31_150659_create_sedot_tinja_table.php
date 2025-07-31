7b98d43babea5727c9e2d7ff49999880336e5ff6
Gust05-art <gstdhika@gmail.com>
2025-07-31 17:08:16 +0100

    update tabel sedot tinja

--- database/migrations/2025_07_31_150659_create_sedot_tinja_table.php
+++ database/migrations/2025_07_31_150659_create_sedot_tinja_table.php
@@ -13,6 +13,18 @@ return new class extends Migration
     {
         Schema::create('sedot_tinja', function (Blueprint $table) {
             $table->id();
+            $table->string('nama_pelanggan', 150);
+            $table->string('nomor_telepon_pelanggan', 15);
+            $table->text('alamat');
+            $table->decimal('longitude', 10, 7)->nullable();
+            $table->decimal('latitude', 9, 7)->nullable();
+            $table->unsignedSmallInteger('kabkota');
+            $table->unsignedSmallInteger('kecamatan');
+            $table->unsignedSmallInteger('kelurahan');
+            $table->string('jenis_bangunan', 20);
+            $table->unsignedSmallInteger('nomor_bangunan');
+            $table->unsignedSmallInteger('rt');
+            $table->enum('status_pengerjaan', ['Belum dikerjakan', 'Sedang dikerjakan', 'Sudah dikerjakan'])->default('Belum dikerjakan');
             $table->timestamps();
         });
     }
