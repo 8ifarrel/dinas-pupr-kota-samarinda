89eecfe41a5cb2d6912c54cb9bdb34278f93fc9f
8ifarrel <farrelsirah@gmail.com>
2025-08-02 22:23:49 +0800

    ganti icon aside jalan peduli

--- resources/views/admin/components/aside.blade.php
+++ resources/views/admin/components/aside.blade.php
@@ -28,7 +28,7 @@
         <button type="button"
           class="flex items-center w-full p-2 text-base text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
           aria-controls="dropdown-jalan-peduli" data-collapse-toggle="dropdown-jalan-peduli">
-          <i class="fa-regular fa-circle-question"></i>
+          <i class="fa-solid fa-road"></i>
           <span class="flex-1 ms-3 text-left rtl:text-right whitespace-nowrap">Jalan Peduli</span>
           <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
